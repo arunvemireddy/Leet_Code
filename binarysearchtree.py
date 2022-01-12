71a4daaf6cda4f97b445f2eda4fed42764484e31
arunvemireddy <vemireddyarun0701@gmail.com>
2022-01-11 22:04:48 -0700

    binary search tree

---
+++ binarysearchtree.py
@@ -0,0 +1,39 @@
+class BST:
+    def __init__(self,key):
+        self.key = key
+        self.lchild = None
+        self.rchild = None
+    
+    def insert(self,data):
+        if self.key is None:
+            self.key=data
+            return
+        if self.key == data:
+            return
+        if self.key>data:
+            if self.lchild:
+                self.lchild.insert(data)
+            else:
+                self.lchild = BST(data)
+        else:
+            if self.rchild:
+                self.rchild.insert(data)
+            else:
+                self.rchild = BST(data)
+    def dispay(self):
+        value = self
+        while value is not None:
+            print(value.key)
+            value=value.lchild
+        value = self.rchild
+        while value is not None:
+            print(value.key)
+            value=value.rchild
+
+root = BST(20)
+list1 = [30,10,5,40,70]
+print(list1)
+for i in list1:
+    root.insert(i)
+
+root.dispay()
