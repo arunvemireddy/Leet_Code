5c0de340b71731c859735b6003603319608b92b0
arunvemireddy <vemireddyarun0701@gmail.com>
2023-01-25 12:34:44 -0700

    fc

--- fibonaccirecurssion.py
+++ fibonaccirecurssion.py
@@ -1,24 +1,23 @@
 class Solution:
+    def __init__(self):
+        print("enter number")
+        x = input()
+        x = int(x)
+        res = self.meth(x)
+        print(res)
+
     def meth(self,n):
+
+        if n==0:
+            result = 0
+
         if n==1 or n==2:
             result = 1
         else:
             result = self.meth(n-1)+self.meth(n-2)
+            
         return result
 
 obj = Solution()
-x=obj.meth(6)
-print(x)
 
 
-class Prctice1:
-    def meth1(self,n):
-        if n==1 or n==2:
-            result =1
-        else:
-            result = self.meth1(n-1)+self.meth1(n-2)
-        return result
-
-op = Prctice1()
-n=5
-print(op.meth1(n))
--- lamda.py
+++ lamda.py
@@ -1,7 +1,4 @@
-# x = lambda a:a+1
-# print(x(4))
-# y = lambda a, b:a+b
-# print(y(1,2))
+# lamda can take any number of arguments but can have only one expression
 
 def test(b):
     return lambda a:a*b
@@ -9,5 +6,6 @@ def test(b):
 x = test(5)
 print(x(6))
 
-import requests
-print(dir(requests))
+
+y = lambda a, b ,c : (a * b)/c
+print(y(1,2,3))
